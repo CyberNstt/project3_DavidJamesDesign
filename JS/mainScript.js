70ed608331bd207f07a60d2711491360f453dcba
CyberNstt <122619691+CyberNstt@users.noreply.github.com>
2023-03-24 14:13:04 +0300

    Update mainScript.js

--- JS/mainScript.js
+++ JS/mainScript.js
@@ -1,6 +1,18 @@
 //BackgroundChanging
-let headerBg = document.querySelector('.Header');
 
+function active() {
+	let sliderZone = document.querySelector('.Header'),
+		sliderActiveBtn = document.querySelectorAll('.HeaderLimiterSliderBtn');
+	sliderZone.addEventListener('click', (active) => {
+		const targetS = active.target;
+		if (targetS.classList.contains('HeaderLimiterSliderBtn')) {
+			sliderActiveBtn.forEach(sActiveBtn => sActiveBtn.classList.remove('HeaderLimiterSliderBtnActive'));
+			targetS.classList.add('HeaderLimiterSliderBtnActive');
+		}
+	})
+}
+active()
+let headerBg = document.querySelector('.Header');
 function firstBackgroundF() {
 	headerBg.classList.remove('HeaderBg3');
 	headerBg.classList.remove('HeaderBg2');
@@ -26,7 +38,7 @@ let filterBlock = document.querySelector(".WorksLimiterFlexLeftHeadFilter"), 	//
 function filter() { 																													// функция события фильтр
 	filterBlock.addEventListener("click", (event) => { 													// добавление контейнеру события с названием "event" и типов "click"
 		const targetF = event.target.dataset["f"];																// объявление константы со значением, цепляющим объекты(из области события) с data-"f"
-		let activeTarget = event.target																						// объявление переменной со значением, цепляющим объекты(из области события)
+		let activeTarget = event.target;																					// объявление переменной со значением, цепляющим объекты(из области события)
 
 		if (activeTarget.classList.contains('worksFilterBtn')) {									// если у любого объекта из области есть класс "...", то
 			filterActiveBtn.forEach(activeBtn => activeBtn.classList.remove('worksButtonActive')); // для каждого такого объекта срабатывает функция "activeBTn", убирающая класс "эктив" ...
@@ -68,14 +80,3 @@ function getItems(className) {
 		}
 	});
 }
-/*
-// starRating Function
-const ratingItemList = document.querySelectorAll('.DavidLimiterStarsButton');
-const ratingItemsArray = Array.prototype.slice.call(ratingItemList);
-
-ratingItemsArray.forEach(item =>
-	item.addEventListener('click', () =>
-		item.parentNode.dataset['totalRatingBtn'] = item.dataset['ratingBtn']
-	)
-);
-*/
