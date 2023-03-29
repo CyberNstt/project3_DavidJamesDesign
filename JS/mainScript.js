cc8bd185764ede75d8009119a3233df8a14658e7
CyberNstt <122619691+CyberNstt@users.noreply.github.com>
2023-03-29 15:02:54 +0300

    Update mainScript.js

--- JS/mainScript.js
+++ JS/mainScript.js
@@ -80,3 +80,41 @@ function getItems(className) {
 		}
 	});
 }
+
+
+
+//Animation on scroll
+
+const animItems = document.querySelectorAll('.ScrollAnimation');
+
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
+	function animOnScroll() {
+		for (let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 4;
+
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+
+			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+				animItem.classList.add('AnimOnScrollActive');
+			} else {
+				if (!animItem.classList.contains('AnimOnScrollNoBack')) {
+					animItem.classList.remove('AnimOnScrollActive');
+				}
+			}
+		}
+	}
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+	}
+	animOnScroll()
+}
